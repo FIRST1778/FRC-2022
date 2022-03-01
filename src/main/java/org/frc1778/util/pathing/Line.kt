33ffc6665720ef68acd9174b756436bf097cbdfa
Gavin Leach <34695615+gamingnight5965@users.noreply.github.com>
2022-02-28 20:28:03 -0800

    Continue Work on Curves

--- src/main/java/org/frc1778/util/pathing/Line.kt
+++ src/main/java/org/frc1778/util/pathing/Line.kt
@@ -6,7 +6,7 @@ import kotlin.math.*
 
 
 class Line(private var length: Double, private var angle: SIUnit<Radian>) {
-    constructor(x: Double, y: Double) : this(sqrt((x - 0.0).pow(2) + (y - 0.0).pow(2)), SIUnit<Radian>(atan(x/y)))
+//    constructor(x: Double, y: Double) : this(sqrt((x - 0.0).pow(2) + (y - 0.0).pow(2)), SIUnit<Radian>(atan(x/y)))
     constructor(x1: Double, y1: Double, x2: Double, y2: Double): this(sqrt((x2 - x1).pow(2) + (y2 - y1).pow(2)), SIUnit<Radian>(atan((x2-x1)/(y2-y1))))
 
 }
